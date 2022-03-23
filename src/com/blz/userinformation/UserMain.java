9227395b81d99c52d9c2458898163f4ddb64620f
LDeepakReddy <l.deepakreddy.com>
2022-03-24 04:09:02 +0530

    Added feature to validate firstname pattern

--- src/com/blz/userinformation/UserMain.java
+++ src/com/blz/userinformation/UserMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UserMain {
     public static void main(String[] args) {
         System.out.println("Welcome to the user information program");
-        
+        System.out.println("Enter first name");
         UserRegistration validate= new UserRegistration();
         Scanner scan = new Scanner(System.in);
         String firstName = scan.next();
