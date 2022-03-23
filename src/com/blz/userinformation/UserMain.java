5684fa6b1943e6ad22c76fa671b1ea9693f3e7e3
LDeepakReddy <l.deepakreddy.com>
2022-03-24 04:30:19 +0530

    Added feature to validate lastname

--- src/com/blz/userinformation/UserMain.java
+++ src/com/blz/userinformation/UserMain.java
@@ -6,10 +6,15 @@ public class UserMain {
     public static void main(String[] args) {
         System.out.println("Welcome to the user information program");
         System.out.println("Enter first name");
-        UserRegistration validate= new UserRegistration();
+        UserRegistration compare = new UserRegistration();
         Scanner scan = new Scanner(System.in);
         String firstName = scan.next();
-        validate.validateFirstName(firstName);
+        System.out.println("Enter last name");
+        String lastName = scan.next();
+        compare.validateFirstName(firstName);
+        compare.validateLastName(lastName);
+
+
     }
 }
 
--- src/com/blz/userinformation/UserRegistration.java
+++ src/com/blz/userinformation/UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     public final String firstName = "^[A-Z]{1}[a-z]{2,}$";
+    public final String lastName ="^[A-Z]{1}[a-z]{3,}$";
 
     public void validateFirstName(String userFirstName) {
         Pattern pattern = Pattern.compile(firstName);
@@ -15,6 +16,15 @@ public class UserRegistration {
         else
             System.out.println("firstName " + userFirstName + " is invalid");
     }
+    public void validateLastName(String userlastName){
+        Pattern pattern = Pattern.compile(lastName);
+        Matcher matcher = pattern.matcher(userlastName);
+        if (matcher.matches())
+            System.out.println("Entered lastname " +userlastName + "is valid");
+        else
+            System.out.println("Entered lastname " +userlastName + "is in valid");
+    }
+
 }
 
 
