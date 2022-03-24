c23903050111eeb9b898e3f3efea48774b97eaec
LDeepakReddy <l.deepakreddy.com>
2022-03-25 02:53:38 +0530

    Added  feature to check the password with special character

--- src/com/blz/userinformation/UserMain.java
+++ src/com/blz/userinformation/UserMain.java
@@ -6,28 +6,41 @@ public class UserMain {
     public static void main(String[] args) {
         System.out.println("Welcome to the user information program");
         UserRegistration compare = new UserRegistration();
-
-        System.out.println("Enter first name");
         Scanner scan = new Scanner(System.in);
-        String firstName = scan.next();
+        System.out.println("1 to validate first name");
+        System.out.println("2 to validate last name");
+        System.out.println("3 to validate email id");
+        System.out.println("4 to validate mobile number");
+        System.out.println("5 to validate Password");
+        int chooseOption = scan.nextInt();
 
-        System.out.println("Enter last name");
-        String lastName = scan.next();
+        switch (chooseOption) {
+            case 1:
+                System.out.println("Enter first name");
+                String firstName = scan.next();
+                compare.validateFirstName(firstName);
+                break;
+            case 2:
+                System.out.println("Enter last name");
+                String lastName = scan.next();
+                compare.validateLastName(lastName);
+                break;
+            case 3:
+                System.out.println("Enter email id");
+                String emailId = scan.next();
+                compare.validateEmail(emailId);
+                break;
+            case 4:
+                System.out.println("Enter mobile number");
+                String mobileNUmber = scan.next();
+                compare.validateMobilenumber(mobileNUmber);
+                break;
+            case 5:
+                System.out.println("Enter password");
+                String passWord = scan.next();
+                compare.validatePassword(passWord);
 
-        System.out.println("Enter email id");
-        String emailId = scan.next();
-
-        System.out.println("Enter mobile number");
-        String mobileNUmber = scan.next();
-
-        System.out.println("Enter password");
-        String passWord = scan.next();
-
-        compare.validateFirstName(firstName);
-        compare.validateLastName(lastName);
-        compare.validateEmail(emailId);
-        compare.validateMobilenumber(mobileNUmber);
-        compare.validatePassword(passWord);
+        }
 
 
     }
